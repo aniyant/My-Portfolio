941b2ed0f64e2ff24799bdb6e35038dded88d2df
aniyant <kumarsunny30066@gmail.com>
2024-11-07 21:51:47 +0530

    done

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,21 +1,4 @@
 export const Projects = () => {
     return <div>Projects
-        <h2>Project 1</h2>
-        <p>Description of Project 1</p>
-        <h2>Project 2</h2>
-        <p>Description of Project 2</p>
-        <h2>Project 3</h2>
-        <p>Description of Project 3</p>
-        <h2>Project 4</h2>
-        <p>Description of Project 4</p>
-        <h2>Project 5</h2>
-        <p>Description of Project 5</p>
-        <h2>Project 6</h2>
-        <p>Description of Project 6</p>
-        <h2>Project 7</h2>
-        <p>Description of Project 7</p>
-        <h2>Project 8</h2>
-        <p>Description of Project 8</p>
-        <h2>Project 9</h2>
     </div>
 }
