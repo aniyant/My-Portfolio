609963eca35d1aeb293d25b5a2efe7b44bc30a39
aniyant <kumarsunny30066@gmail.com>
2024-12-02 20:26:21 +0530

    done

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <div>My Portfoliosdfffffffffffffffffffffffffff</div>
+      <div>My Portfolio</div>
       </header>
     </div>
   );
