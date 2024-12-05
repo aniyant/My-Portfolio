d1baf97a936aaceba4f3eebab533cc68b92ed949
aniyant <kumarsunny30066@gmail.com>
2024-12-05 19:36:22 +0530

    done

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -10,4 +10,8 @@ export const Projects = () => {
         <h2>Project 2</h2>
         <p>Description of Project 2</p>
     </div>
+    <div>
+        lkjsdlf;j
+    </div>
+    <Projects>Project</Projects>
 }
