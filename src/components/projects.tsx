8d041baa324e8fb55eed24ba0f1f937067977f6a
aniyant <kumarsunny30066@gmail.com>
2024-12-05 19:35:29 +0530

    done

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,3 +1,13 @@
 export const Projects = () => {
     return <div>Projects</div>
+    Add your projects here
+    For example:
+    <div>
+        <h2>Project 1</h2>
+        <p>Description of Project 1</p>
+    </div>
+    <div>
+        <h2>Project 2</h2>
+        <p>Description of Project 2</p>
+    </div>
 }
